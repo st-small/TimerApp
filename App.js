5b37a72bb56a11dc738c89970ba88d044162149e
Stanly Shiyanovskiy <st-small@rambler.ru>
2018-05-24 23:14:56 +0300

    added tableView with results and sorted them

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
 import moment from 'moment'
 
 const DATA = {
@@ -7,18 +7,81 @@ const DATA = {
   laps: [12345, 2345, 34567, 98765],
 }
 
-function Timer({ interval }) {
+function Timer({ interval, style }) {
   const duration = moment.duration(interval)
   const centiseconds = Math.floor(duration.milliseconds() / 10)
-  return <Text style={styles.timer}>
-    {duration.minutes()}:{duration.seconds()},{centiseconds}</Text>
+  return (
+    <Text style={style}>
+      {duration.minutes()}:{duration.seconds()},{centiseconds}
+    </Text>
+  )
+}
+
+function RoundButton({ title, color, background }) {
+  return (
+    <View style={[ styles.button, { backgroundColor: background }]}>
+      <View style={styles.buttonBorder}>
+        <Text style={[ styles.buttonTitle, {color}]}>{title}</Text>
+      </View>
+    </View>
+  )
+}
+
+function Lap({ number, interval, fastest, slowest }) {
+  const lapStyle = [
+    styles.lapText,
+    fastest && styles.fastest,
+    slowest && styles.slowest,
+  ]
+  return (
+    <View style={styles.lap}>
+      <Text style={lapStyle}>Lap {number}</Text>
+      <Timer style={lapStyle} interval={interval}/>
+    </View>
+  )
+}
+
+function LapsTable({ laps }) {
+  const finishedLaps = laps.slice(1)
+  let min = Number.MAX_SAFE_INTEGER
+  let max = Number.MIN_SAFE_INTEGER
+  if (finishedLaps.length >= 2) {
+    finishedLaps.forEach(lap => {
+      if (lap < min) min = lap
+      if (lap > max) max = lap
+    })
+  }
+  return (
+    <ScrollView style={styles.scrollView}>
+      {laps.map((lap, index) => (
+        <Lap
+          number={laps.length - index}
+          key={laps.length - index}
+          interval={lap}
+          fastest={lap == min}
+          slowest={lap == max}
+        />
+      ))}
+    </ScrollView>
+  )
+}
+
+function ButtonsRow({ children }) {
+  return (
+    <View style={styles.buttonsRow}>{children}</View>
+  )
 }
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Timer interval={DATA.timer}/>
+        <Timer interval={DATA.timer} style={styles.timer}/>
+        <ButtonsRow>
+          <RoundButton title='Reset' color='#ffffff' background='#3d3d3d' />
+          <RoundButton title='Start' color='#50d167' background='#1b361f' />
+        </ButtonsRow>
+        <LapsTable laps={DATA.laps} />
       </View>
     );
   }
@@ -30,10 +93,56 @@ const styles = StyleSheet.create({
     backgroundColor: '#0d0d0d',
     alignItems: 'center',
     paddingTop: 130,
+    paddingHorizontal: 20,
   },
   timer: {
     color: '#FFFFFF',
     fontSize: 76,
     fontWeight: '200',
-  }
+  },
+  button: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonTitle: {
+    fontSize: 18,
+  },
+  buttonBorder: {
+    width: 76,
+    height: 76,
+    borderRadius: 38,
+    borderWidth: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonsRow: {
+    flexDirection: 'row',
+    alignSelf: 'stretch',
+    justifyContent: 'space-between',
+    marginTop: 80,
+    marginBottom: 30,
+  },
+  lapText: {
+    color: '#ffffff',
+    fontSize: 18,
+  },
+  lap: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderColor: '#151515',
+    borderTopWidth: 1,
+    paddingVertical: 10,
+  },
+  scrollView: {
+    alignSelf: 'stretch',
+  },
+  fastest: {
+    color: '#4bc05f',
+  },
+  slowest: {
+    color: '#cc3531',
+  },
 })
